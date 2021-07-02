cf345c14a9809252752402c5effa55c23ad39b7e
Ryan Erricson <ryanerricson@gmail.com>
2021-07-02 12:29:06 -0400

    Created Nav and Home Components

---
+++ src/components/NavBar.js
@@ -0,0 +1,20 @@
+import { Breadcrumbs, Link, Typography } from '@material-ui/core';
+import React from 'react';
+
+
+const NavBar = () => {
+
+    let handleClick = () => {
+        console.log("I was clicked")
+    }
+
+    return (
+        <div>
+            <Typography >
+
+            </Typography>
+        </div>
+    );
+}
+
+export default NavBar;
