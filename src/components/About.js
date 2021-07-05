02cc5875b2eeeb50c150e1cd3eeac0026e55f7a6
Ryan Erricson <ryanerricson@gmail.com>
2021-07-05 15:59:39 -0400

    Made contact links clickable

--- src/components/About.js
+++ src/components/About.js
@@ -3,13 +3,14 @@ import React from 'react';
 const About = () => {
     return (
         <div className="about-section">
+        <div className="icon-div">
             <h1>RYAN ERRICSON</h1>
             <p>New developer, this will be my about section. Hire me.<br/>
              woriughforuighowrhbgforwiejgpqeojgw </p>
-             <div className="icon-div">
+             
              <h3>Check Out My Stack!</h3>
              <i class="devicon-react-original-wordmark colored"></i>
-          <i class="devicon-javascript-plain colored"></i>
+            <i class="devicon-javascript-plain colored"></i>
           <i class="devicon-ruby-plain colored"></i><br />
           <i class="devicon-rails-plain-wordmark colored"></i>
           <i class="devicon-redux-original colored"></i>
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,15 +5,18 @@ import FacebookIcon from '@material-ui/icons/Facebook';
 import BookIcon from '@material-ui/icons/Book';
 import EmailIcon from '@material-ui/icons/Email';
 
+
 const Contact = () => {
     return (
-        <div>
+        <div className="contact-div">
+        <div className="sidebar">
         <h1>Ryan Erricson</h1>
-            <GitHubIcon />
-            <LinkedInIcon />
-            <FacebookIcon />
-            <BookIcon />
-            <EmailIcon />
+        <p>Feel free to contact me at any of the links posted below. I am currently looking for </p>
+            <a href="https://github.com/rickySONZ"><GitHubIcon /></a>
+            <a href="https://www.linkedin.com/in/ryan-erricson-992890113/"><LinkedInIcon /></a>
+            <a href="https://www.facebook.com/ryan.erricson"><FacebookIcon /></a>
+            <a href="https://dev.to/rickysonz"><BookIcon /></a>
+           <a href='mailto: ryanerricson@gmail.com'><EmailIcon /></a></div>
         </div>
     );
 }
