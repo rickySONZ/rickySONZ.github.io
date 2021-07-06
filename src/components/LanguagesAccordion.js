b698db8a2747bd6cd8aed5a0c547ec3230a4d3ae
Ryan Erricson <ryanerricson@gmail.com>
2021-07-06 15:02:26 -0400

    Styled divs and images to have a blur effect

--- src/components/LanguagesAccordion.js
+++ src/components/LanguagesAccordion.js
@@ -20,23 +20,33 @@ export default function LanguagesAccordion() {
   const classes = useStyles();
 
   return (
+      <div className="accordion-div" >
     <div className={classes.root}>
-      <Accordion style={{backgroundColor: 'rgba(0, 0, 0, 0.5)', width: '70%', justifySelf: 'center', margin: '2% auto'}}>
+      <Accordion style={{backgroundColor: 'rgba(0, 0, 0, 0.5)', width: '70%', justifySelf: 'center', margin: '2% auto', textAlign: 'center'}}>
         <AccordionSummary
           expandIcon={<ExpandMoreIcon />}
           aria-controls="panel1a-content"
           id="panel1a-header"
         >
-          <Typography className={classes.heading}>Languages and Frameworks</Typography>
+          <Typography className={classes.heading} style={{justify: 'center', }}>Languages and Frameworks</Typography>
         </AccordionSummary>
         <AccordionDetails>
           <Typography>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
-            sit amet blandit leo lobortis eget.
+          <i class="devicon-react-original-wordmark colored"></i>
+            <i class="devicon-javascript-plain colored"></i>
+          <i class="devicon-ruby-plain colored"></i><br></br>
+          <i class="devicon-rails-plain-wordmark colored"></i>
+          <i class="devicon-redux-original colored"></i>
+          <i class="devicon-postgresql-plain-wordmark colored"></i>
+          <i class="devicon-git-plain-wordmark colored"></i><br></br>
+          <i class="devicon-html5-plain-wordmark colored"></i>
+          <i class="devicon-css3-plain-wordmark colored"></i>
+          <i class="devicon-heroku-original-wordmark colored"></i>
           </Typography>
         </AccordionDetails>
       </Accordion>
       
+    </div>
     </div>
   );
 }
