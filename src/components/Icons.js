17dc65a530ccfaf590fdffe40289c53f13196ca5
Ryan Erricson <ryanerricson@gmail.com>
2021-07-10 11:43:19 -0400

    reformatted nav with media queries

--- src/components/Icons.js
+++ src/components/Icons.js
@@ -4,16 +4,16 @@ const Icons = () => {
     return (
         <div className="icons">
         <h1>Languages and Frameworks</h1>
-            <i class="devicon-react-original-wordmark colored"></i>
-            <i class="devicon-javascript-plain colored"></i>
-          <i class="devicon-ruby-plain colored"></i><br></br>
-          <i class="devicon-rails-plain-wordmark colored"></i>
-          <i class="devicon-redux-original colored"></i>
-          <i class="devicon-postgresql-plain-wordmark colored"></i>
-          <i class="devicon-git-plain-wordmark colored"></i><br></br>
-          <i class="devicon-html5-plain-wordmark colored"></i>
-          <i class="devicon-css3-plain-wordmark colored"></i>
-          <i class="devicon-heroku-original-wordmark colored"></i>
+            <i className="devicon-react-original-wordmark colored"></i>
+            <i className="devicon-javascript-plain colored"></i>
+          <i className="devicon-ruby-plain colored"></i><br></br>
+          <i className="devicon-rails-plain-wordmark colored"></i>
+          <i className="devicon-redux-original colored"></i>
+          <i className="devicon-postgresql-plain-wordmark colored"></i>
+          <i className="devicon-git-plain-wordmark colored"></i><br></br>
+          <i className="devicon-html5-plain-wordmark colored"></i>
+          <i className="devicon-css3-plain-wordmark colored"></i>
+          <i className="devicon-heroku-original-wordmark colored"></i>
         </div>
     );
 }
